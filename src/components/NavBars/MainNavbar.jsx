3d61cd36ec5293237902b06b86f8237839b79e9b
Aditya sharma <>
2025-04-02 10:28:09 +0530

    minor fixes

--- src/components/NavBars/MainNavbar.jsx
+++ src/components/NavBars/MainNavbar.jsx
@@ -1,12 +1,12 @@
 const MainNavbar = ({ NavArray }) => {
   return (
-    <div className="flex justify-between mt-[24px] items-center">
+    <div className="flex justify-between mt-[24px] items-center ">
       {NavArray?.map((nav, index) => (
         <div
           key={nav?.text}
           className="border-2 flex flex-col p-[24px] gap-[24px] justify-center items-center
             w-[230px] h-[136px] transition-all duration-300 
-            hover:bg-[#F3F8FF] hover:text-[#2B5592] hover:h-[186px] cursor-pointer"
+            hover:bg-[#F3F8FF] hover:text-[#2B5592] hover:scale-125 cursor-pointer"
         >
           <span className="text-[#7B8493] hover:text-[#2B5592]" >{nav?.icon}</span>
           <p>{nav?.label}</p>

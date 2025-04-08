59fdef8c61d9eb23580ccabd0a92c87247ee267b
Aditya sharma <>
2025-04-08 12:53:04 +0530

    team section fix

--- src/Pages/AboutUs/Components/TeamSection.jsx
+++ src/Pages/AboutUs/Components/TeamSection.jsx
@@ -40,15 +40,15 @@ const TeamSection = () => {
           <h2 className="text-center w-[70%]">The steadfast strength and unity of our team.</h2>
         </div>
         <div className="grid grid-cols-2 gap-[32px]">
-          <div className="grid grid-cols-2 gap-[32px]">
+          <div className="grid grid-cols-2 gap-[32px] ">
             {profileArray?.map((prof) =>
               <div
                 key={prof.name}
                 onClick={() => setActiveProfile(prof)}
+                className="w-full aspect-square cursor-pointer"
               ><img
-
                   src={prof.image} alt="anil"
-                  className="w-full h-[300px] rounded-[4px]"
+                  className="w-full h-full rounded-[4px]"
                 />
                 <h5 className="font-[700] text-[24px] leading-[32px] mt-[16px]" >{prof?.name}</h5>
                 <p className="font-[400] text-[16px] leading-[28px] text-[#414750] mt-[10px]">{prof?.desg}</p>
@@ -56,14 +56,14 @@ const TeamSection = () => {
               </div>
             )}
           </div>
-          <div className="relative">
+          <div className="relative w-full aspect-[7/8] rounded-[4px] overflow-hidden">
             <img
               src={activeProfile?.image}
               alt="active profile"
-              className="w-full h-full "
+              className="w-full h-full object-cover"
             />
-            <div className="absolute bottom-0 w-full h-full bg-[#00000080]" />
-            <div className="absolute bottom-20 text-white ps-[40px] pe-[60px] ">
+            <div className="absolute bottom-0 w-full h-full bg-[#00000080] bg-opacity-50" />
+            <div className="absolute bottom-[32px] text-white px-[32px] ">
               <h3 className="mb-[32px] text-white font-[700] text-[40px] leading-[56px] ">{activeProfile?.name}</h3>
               <p className=" font-[400] text-[16px] leading-[28px]">{activeProfile?.desc}</p>
             </div>
